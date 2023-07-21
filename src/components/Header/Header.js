636143832c9ae2f241013e091da188dc8d1e733e
atishay_25 <codesome.aj.25@gmail.com>
2023-07-21 23:19:29 +0530

    3dObject

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import { motion } from 'framer-motion'
+import { ComputersCanvas } from '../canvas'
 const Header = () => {
   return (
     <header>
       <div className="container header_container">
-        <h5>Hi I'm</h5>
-        <h1>Atishay Jain</h1>
-        <h5 className='text-light'>Fullstack Developer</h5>
-        <CTA/>
+        <div className='d-flex design '>
+          <div className='d-flex flex-column design1 '>
+            <div className='dot' />
+            <div className='line' />
+          </div>
+          <div>
+            <h1>Hi I'm <span style={{ color: '#915eff' }}>Atishay</span></h1>
+            <h5 className='text-light'>Fullstack Developer</h5>
+            <CTA />
+          </div>
+        </div>
+
+
+      <ComputersCanvas/>
       </div>
 
     </header>
