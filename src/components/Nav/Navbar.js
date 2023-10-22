a70d65ee9b1b06ea018c094a3da2ff379cc6e04a
atishay_25 <codesome.aj.25@gmail.com>
2023-10-22 12:39:30 +0530

    gggg

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -4,10 +4,10 @@ import {AiOutlineHome ,AiOutlineUser} from 'react-icons/ai'
 import {BiBook,BiMessageSquareDetail} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
 const Navbar = () => {
-  const [navActive , setNavActive] = useState('#');
+  const [navActive , setNavActive] = useState('#home');
   return (
     <navbar>
-      <a href="#" onClick={()=>{setNavActive('#')}} className={navActive==='#'?'active':''}><AiOutlineHome/></a>
+      <a href="#home" onClick={()=>{setNavActive('#home')}} className={navActive==='#home'?'active':''}><AiOutlineHome/></a>
       <a href="#about" onClick={()=>{setNavActive('#about')}} className={navActive==='#about'?'active':''}><AiOutlineUser/></a>
       <a href="#experience" onClick={()=>{setNavActive('#experience')}} className={navActive==='#experience'?'active':''}><BiBook/></a>
       <a href="#portfolio" onClick={()=>{setNavActive('#services')}} className={navActive==='#services'?'active':''}><RiServiceLine/></a>
